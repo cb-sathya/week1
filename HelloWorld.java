d175047847f670df39914aa16c29e43e180d3cb0
cb-sathya <sathya@chargebee.com>
2015-12-28 13:20:11 +0530

    HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/** 
+*This class implements an application that displays the message "HELLOWORLD!" on the *output screen
+*/
+
+class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("HELLOWORLD!");		//prints the message
+	}
+}
